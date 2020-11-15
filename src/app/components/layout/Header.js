4c54644b2b9869f8746ca728a04e3f2a99ba8d35
Cedric De Blanck <cedricdeblanck@MacBook-Pro-van-Cedric.local>
2020-11-15 22:39:15 +0100

    wip

--- src/app/components/layout/Header.js
+++ src/app/components/layout/Header.js
@@ -1,22 +1,13 @@
 import { NavLink } from "react-router-dom";
 import * as Routes from '../../routes';
 import '@animated-burgers/burger-squeeze/dist/styles.css' 
-import { Power1, Power3, TimelineMax } from "gsap";
 const checkActive = (match, location) => {
     if(!location) return false;
     const {pathname} = location;
     return pathname === "/";
 } 
 
-const hero = document.querySelector('.hero');
-// const slider = document.querySelector('.slider');
-// const logo = document.querySelector('#logo');
-// const hamburger = document.querySelector('.hamburger');
-// const headline = document.querySelector('.headline');
 
-const tl = new TimelineMax();
-tl.fromTo(hero, 1, { height: "0%"} ,{ height: "60%", ease: Power1.easeInOut})
-.fromTo(hero, 1.2, {width: "0%"}, { width: "30%", ease: Power3.easeInOut});
 
 const Header = () => {
     return (
--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -1,28 +1,90 @@
 import face from "../assets/images/face.jpg";
+import logo from "../assets/images/logo.svg";
+import { Header } from "../components/layout"; 
+import { useState } from "react";
+import styled from 'styled-components';
+import 'animate.css';
+
+const Hamburger = styled.div`
+    width: 2rem;
+    height: 2rem;
+    position: fixed;
+    top: 15px;
+    right: 20px;
+    display: flex;
+    justify-content: space-around;
+    flex-flow: column nowrap;
+    z-index: 5;
+    cursor: pointer;
+
+    div {
+        width: 2rem;
+        height: 0.25rem;
+        background-color: white;
+        border-radius: 10px;
+        transform-origin: 1px;
+        transition-duration: 0.3s;
+
+        &:nth-child(1) {
+            transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
+        }
+
+        &:nth-child(2) {
+            transform: ${({ open }) => open ? 'translateX(150%)' : 'translateX(0)'};
+            opacity: ${({ open }) => open ? '0' : '1'};
+            margin-left: 5px;
+        }
+
+        &:nth-child(3) {
+            transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
+        }
+    }
+
+`
+
+const Navigation = styled.div`
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    background: #111;
+    top: 0;
+    left: 0;
+    transition-duration: 0.3s;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
+    z-index: 2;
+`
+
 const Home = () => {
+
+    const [open, setOpen] = useState(false);
 
     return (
         <header>
             <nav>
-                <h3 id="logo">Dream Vacation</h3>
-                <div className="hamburger">
-                    <button onClick={(e) => {
-                        e.preventDefault();                    
-                    }} className="hamburger hamburger--elastic" type="button">
-                        <span className="hamburger-box">
-                        <span className="hamburger-inner"></span>
-                        </span>
-                    </button>
-                </div>
+                
+                <Hamburger open={open} onClick={() => { setOpen(!open); }}>
+                    <div />
+                    <div />
+                    <div />
+                </Hamburger>
+
+                <Navigation className="navigation" open={open}>
+                    <div>
+                        <nav>
+                            <Header />
+                        </nav>
+                    </div>
+                </Navigation>
             </nav>
-            <section>
-                <div className="hero">
-                    <img src={face} alt="Me"></img>
+            <section className="animate__fadeInLeft">
+                <div className="photo--section">
                     <h1 className="headline">Cédric De Blanck</h1>
                 </div>
             </section>
-
-            <div className="slider"></div>
+            <div className="mouse-scroll">
+                <div className="mouse-line"></div>
+            </div>
+            <div className="background"><img className="animate__bounceInLeft" id="logo" src={logo}></img></div>
         </header>
     );
 };
